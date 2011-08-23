baf8f2406e26c8dfc40316aa84b1aab88373bdfd
Matheus Ribeiro <mfribeiro@gmail.com>
2011-08-23 01:09:42 -0300

    passei os valores do personagem pra variavbel global

--- planilha.js
+++ planilha.js
@@ -1,8 +1,53 @@
 goog.require('goog.dom');
 
-var id_atributos = ['for', 'des', 'con', 'int', 'sab', 'car'];
+var valor_base = "-valor-base";
+var valor_total = "-valor-total";
+var mod_total = "-mod-total";
 var mod_base = "-mod-base";
 var mod_racial = "-mod-racial";
+
+var modificadores_raciais = {
+	anao: { constituicao: +2, carisma: -2 },
+	halfling: { forca: -2, destreza: +2 },
+	humano: {},
+	elfo: { destreza: +2, constituicao: -2 },
+	gnomo: { forca: -2, constituicao: +2 },
+	meioelfo: {},
+	meioorc: { forca: +2, inteligencia: -2, carisma: -2 }
+};
+
+// Valores do personagem.
+var personagem = {
+	raca: "humano",
+	classes: ["guerreiro"],
+	alinhamento: "LB",
+	atributos: {
+		forca: { 
+			valor: { base: 10, racial: 0, total: 10 },
+			modificador: 0
+		},	
+		destreza: { 
+			valor: { base: 10, racial: 0, total: 10 },
+			modificador: 0
+		},	
+		constituicao: { 
+			valor: { base: 10, racial: 0, total: 10 },
+			modificador: 0
+		},	
+		inteligencia: { 
+			valor: { base: 10, racial: 0, total: 10 },
+			modificador: 0
+		},	
+		sabedoria: { 
+			valor: { base: 10, racial: 0, total: 10 },
+			modificador: 0
+		},	
+		carisma: { 
+			valor: { base: 10, racial: 0, total: 10 },
+			modificador: 0
+		}	
+	}
+};
 
 // Sempre que uma mudanca ocorrer, esta funcao sera responsavel por atualizar
 // os dados da planilha.
@@ -14,44 +59,53 @@ function AtualizaGeral() {
 
 // Atualiza os modificadores raciais.
 function AtualizaRaca() {
-	var modificadores_raciais = {
-		anao: { "con": "+2", "car": "-2" },
-		halfling: { "for": "-2", "des": "+2" },
-		humano: {},
-		elfo: { "des": "+2", "con": "-2" },
-		gnomo: { "for": "-2", "con": "+2" },
-		meioelfo: {},
-		meioorc: { "for": "+2", "int": "-2", "car": "-2" }
-	};
-	// limpa todos os modificadores raciais
-	for (var i = 0; i < id_atributos.length; ++i) {
-		var span_mod_racial = goog.dom.getElement(id_atributos[i] + mod_racial);
-		span_mod_racial.innerText = '';
+	// Zera todos os modificadores raciais.
+	for (var habilidade in personagem.atributos) {
+		personagem.atributos[habilidade].valor.racial = 0;
 	}
 	// Aplica os modificadores raciais.
 	var opcoes_raca = goog.dom.getElement("raca").options;
 	var modificadores_raca =
-			modificadores_raciais[opcoes_raca[opcoes_raca.selectedIndex].value];
-	for (var habilidade_modificada in modificadores_raca) {
-		var span_mod_racial = goog.dom.getElement(habilidade_modificada + mod_racial);
-		span_mod_racial.innerText = modificadores_raca[habilidade_modificada];
+		modificadores_raciais[opcoes_raca[opcoes_raca.selectedIndex].value];
+	for (var habilidade in modificadores_raca) {
+		if (habilidade != 'forca') continue;
+		personagem.atributos[habilidade].valor.racial = 
+			modificadores_raca[habilidade];
+	}
+	// Escreve todos os modificadores raciais.
+	for (var habilidade in personagem.atributos) {
+		if (habilidade != 'forca') continue;
+		goog.dom.getElement(habilidade + mod_racial).innerText = 
+			personagem.atributos[habilidade].valor.racial;
 	}
 }
 
 // Atualiza todos os modificadores dos atributos bases (for, des, con, int, sab, car).
 function AtualizaModificadoresAtributos() {
 	// Busca cada elemento das estatisticas e atualiza modificadores.
-	for (var i = 0; i < id_atributos.length; ++i) {
-		// base.
-		var input_atributo = goog.dom.getElement(id_atributos[i]);
-		var valor_modificador_base = Math.floor((input_atributo.value - 10) / 2);
-		goog.dom.getElement(id_atributos[i] + mod_base).innerText =  valor_modificador_base;
-		// raca.
-		var span_modificador_racial = goog.dom.getElement(id_atributos[i] + mod_racial);
-		var valor_modificador_racial = parseInt(span_modificador_racial.innerText) || 0;
-		// Atualiza o modificador.
-		goog.dom.getElement(id_atributos[i] + '-mod').innerText =
-			valor_modificador_base + valor_modificador_racial;
+	for (var habilidade in personagem.atributos) {
+		if (habilidade != 'forca') continue;
+		// valor total do atributo.
+		var input_atributo = goog.dom.getElement(habilidade + valor_base);
+		personagem.atributos[habilidade].valor.base = 
+			parseInt(input_atributo.value);
+		personagem.atributos[habilidade].valor.total = 0;
+		for (var modificador_valor in personagem.atributos[habilidade].valor) {
+			if (modificador_valor != 'total') {
+				personagem.atributos[habilidade].valor.total += 
+					personagem.atributos[habilidade].valor[modificador_valor];
+			}
+		}
+		// Escreve o valor total:
+		goog.dom.getElement(habilidade + valor_total).innerText =
+			personagem.atributos[habilidade].valor.total;
+
+		// modificador.
+		personagem.atributos[habilidade].modificador = 
+			Math.floor((personagem.atributos[habilidade].valor.total - 10) / 2);
+		// Escreve o modificador.
+		goog.dom.getElement(habilidade + mod_total).innerText =  
+			personagem.atributos[habilidade].modificador;
 	}
 }
 
