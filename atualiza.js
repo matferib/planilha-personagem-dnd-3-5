c7e77b987ef42fc28a52356054f1ecddf4941f40
Matheus Ribeiro <mfribeiro@gmail.com>
2012-01-03 04:00:19 -0200

    tirei um conflito que ficou

--- atualiza.js
+++ atualiza.js
@@ -474,15 +474,14 @@ function _AtualizaListaArmas() {
   }
 }
 
-<<<<<<< local
 function _AtualizaNotas() {
   Dom('text-area-notas').value = personagem.notas;
-=======
+}
+
 function _AtualizaModoVisao() {
   for (var visao in tabelas_visoes) {
     var span_visao = goog.dom.getElement('span-' + visao);
     span_visao.className = personagem.modo_visao == visao ?
         'selecionado': '';
   }
->>>>>>> other
 }
