7ced98c644ab4f6f6e47200391d0a894b4d5bf12
Fernando C. de Carvalho Neto <fcarvalho@gmail.com>
2012-01-07 02:26:36 -0200

    Adicionado botao para adicionar 5 pontos de dano no campo ferimentos

---
+++ geracao.js
@@ -0,0 +1,95 @@
+// Gera os pontos de vida do personagem de acordo com as classes.
+// @param modo pode ser elite, comum, personagem.
+function GeraPontosDeVida(modo) {
+  if (modo != 'personagem' && modo != 'elite' && modo != 'comum') {
+    alert('Modo ' + modo + ' invalido. Deve ser elite, comum ou personagem.');
+    return;
+  }
+  // Para cada classe, rolar o dado.
+  var total_pontos_vida = 0;
+  // Primeiro eh diferente na elite e personagem.
+  var primeiro = (modo == 'comum') ? false : true;
+  for (var i = 0; i < personagem.classes.length; ++i) {
+    var info_classe = personagem.classes[i];
+    for (var j = 0; j < info_classe.nivel; ++j) {
+      var pontos_vida_nivel = 0;
+      if (primeiro) {
+        if (modo == 'elite') {
+          pontos_vida_nivel = tabelas_classes[info_classe.classe].dados_vida +
+              personagem.atributos.constituicao.modificador;
+        } else {
+          pontos_vida_nivel = Rola(1, tabelas_classes[info_classe.classe].dados_vida) +
+              personagem.atributos.constituicao.valor;
+        }
+        primeiro= false;
+      } else {
+        pontos_vida_nivel = Rola(1, tabelas_classes[info_classe.classe].dados_vida) +
+            personagem.atributos.constituicao.modificador;
+      }
+      // Nunca pode ganhar menos de 1 ponto por nivel.
+      if (pontos_vida_nivel < 1) {
+        pontos_vida_nivel = 1;
+      }
+      total_pontos_vida += pontos_vida_nivel;
+    }
+  }
+  Dom('pontos-vida-total').value = total_pontos_vida;
+}
+
+// Gera os atributos do personagem rolando 3d6 para cada um.
+function GeraAleatorioComum() {
+  var atributos = [ 'forca', 'destreza', 'constituicao', 'inteligencia', 'sabedoria', 'carisma' ];
+  for (var i = 0; i < atributos.length; ++i) {
+    Dom(atributos[i] + '-valor-base').value = Rola(3, 6);
+  }
+}
+
+// Gera os atributos do personagem rolando 4d6 menos o menor.
+function GeraAleatorioElite() {
+  var atributos = [ 'forca', 'destreza', 'constituicao', 'inteligencia', 'sabedoria', 'carisma' ];
+  //var rolagens = [];
+  for (var i = 0; i < atributos.length; ++i) {
+    var total = 0;
+    var menor = 7;
+    for (var j = 0; j < 4; ++j) {
+      var rolagem = Rola(1, 6);
+      //rolagens.push(rolagem);
+      if (rolagem < menor) {
+        menor = rolagem;
+      }
+      total += rolagem;
+    }
+    Dom(atributos[i] + '-valor-base').value = total - menor;
+  }
+}
+
+// Tabelados, valores eh um array de valores a serem usados.
+function _GeraTabelado(valores) {
+  if (personagem.classes.length == 0) {
+    // Nunca deve acontecer.
+    alert('Personagem sem classe');
+    return;
+  }
+
+  var primeira_classe = personagem.classes[0];
+  if (primeira_classe.classe == 'aristocrata' || primeira_classe.classe == 'expert') {
+    alert("É recomendado colocar os valores na mão para aristocratas e experts");
+  }
+
+  for (var i = 0; i < valores.length; ++i) {
+    Dom(
+        tabelas_geracao_atributos[primeira_classe.classe][i]  + '-valor-base').value = 
+            valores[i];
+  }
+}
+
+// Gera os atributos do personagem usando o 'elite array'. Usa a primeira classe.
+function GeraElite() {
+  _GeraTabelado( [ 15, 14, 13, 12, 10, 8 ]);
+}
+
+// Gera os atributos do personagem usando o 'non elite array'. Usa a primeira classe.
+function GeraComum() {
+  _GeraTabelado([ 13, 12, 11, 10, 9, 8 ]);
+}
+
---
+++ personagem.js
@@ -0,0 +1,240 @@
+// Apenas os dados do personagem e funcoes de conversao de entrada para personagem.
+var personagem = {
+  modo_visao: 'completo',
+  nome: '',
+  raca: 'humano',
+  tamanho: { categoria: 'medio', modificador_ataque_defesa: 0 }, 
+  alinhamento: "",
+  experiencia: 0,
+  // Cada entrada: { classe, nivel }.
+  classes: [],
+  pontos_vida: {
+    dados_vida: 0,
+    total: 0,
+    ferimentos: 0,
+    temporarios: 0
+  },
+  atributos: {
+    pontos: {
+      disponiveis: 0,
+      gastos: [],  // cada entrada, um atributo
+    },
+    forca: { 
+      base: 0,
+      bonus_nivel: 0,
+      racial: 0,
+      valor: 0,
+      modificador: 0
+    },  
+    destreza: {
+      base: 0, 
+      bonus_nivel: 0,
+      racial: 0,
+      valor: 0,
+      modificador: 0
+    },  
+    constituicao: { 
+      base: 0, 
+      bonus_nivel: 0,
+      racial: 0,
+      valor: 0,
+      modificador: 0
+    },  
+    inteligencia: { 
+      base: 0, 
+      bonus_nivel: 0,
+      racial: 0,
+      valor: 0,
+      modificador: 0
+    },  
+    sabedoria: { 
+      base: 0, 
+      bonus_nivel: 0,
+      racial: 0,
+      valor: 0,
+      modificador: 0
+    },  
+    carisma: { 
+      base: 0, 
+      bonus_nivel: 0,
+      racial: 0,
+      valor: 0,
+      modificador: 0
+    }
+  },
+  bba: 0,
+  bba_cac: 0,  // inclui tamanho e forca.
+  bba_distancia: 0,  // inclui tamanho e destreza.
+  // talentos
+  talentos: {  
+    // talentos livres.
+    total: 0,  
+    // Cada entrada, { nome, complemento }. 
+    // Ambos podem ser null, pela verificacao de requisito.
+    lista: [],
+    // talentos especificos de classe.
+    // TODO
+  },
+  // pericias.
+  pericias: {
+    // Quantos pontos o personagem pode gastar.
+    total_pontos: 0,
+    // Quantos ele ja gastou.
+    pontos_gastos: 0,
+    // Cada entrada: chave_pericia: { pontos, graduacoes, bonus_sinergia, 
+    // bonus_talentos: { chave: valor }, bonus_habilidade, total }.
+    lista: {},
+  },
+  // Cada entrada: { nome, 
+  //                 arma_primaria: {  nome, 
+  //                                   bonus_por_categoria: { categoria: { ataque, dano }, ... } ] }, 
+  //                 arma_secundaria: { idem }, }.
+  estilos_luta: [],
+  ca: { normal: 10, surpreso: 10, toque: 10 },
+  salvacoes : {
+    fortitude: { base: 0, racial: 0, total: 0 },
+    reflexo: { base: 0, racial: 0, total: 0 },
+    vontade: { base: 0, racial: 0, total: 0 },
+    // Outras salvacoes derivadas.
+  },
+  // Lista de armas que o personagem eh proficiente.
+  // Cada entrada: { nome: true }, onde nome eh o nome da chave. So a presenca do campo eh
+  // suficiente para indicar proficiencia, sem necessidade de um booleano.
+  proficiencia_armas: {},
+  // Cada entrada: { nome: 1|2 }.
+  foco_armas: {},
+  // Cada entrada: 
+  //     { entrada: { chave, bonus, obra_prima }, nome_gerado, bonus_ataque, bonus_dano, 
+  //       proficiente, foco: { maior }, especializado: { maior }, arma_tabela };
+  // O nome_gerado junta o nome com OP ou o bonus. Por exemplo, espada longa +1.
+  // Sempre havera um ataque desarmado aqui.
+  armas: [],
+  // Armadura: { nome, bonus_magico }
+  armadura: {},
+  // Valor pode ser qualquer coisa.
+  equipamentos: '',
+  // Feiticos. Cada entrada: chave_classe: { 
+  //   habilidade_chave, 
+  //   conhecidos: { 0: [], ..., 9 },
+  //   slots: { 0: { base, bonus_atributo, feiticos: [], feitico_dominio  }, ... 9 } }
+  // TODO: transformar em:
+  //   slots: { 0: { base, bonus_atributo, feiticos: [ { nome, gasto} ], feitico_dominio: {nome, gasto}  }, ... 9 } }
+  feiticos: {},
+  moedas: { platina: 0, ouro: 0, prata: 0, cobre: 0 },
+
+  notas: '',
+};
+
+// Encontra uma arma do personagem pelo nome gerado.
+// @return a arma do personagem.
+function ArmaPersonagem(nome_gerado) {
+  for (var i = 0; i < personagem.armas.length; ++i) {
+    var arma = personagem.armas[i];
+    if (arma.nome_gerado == nome_gerado) {
+      return arma;
+    }
+  }
+  return null;
+}
+
+// @return true se o personagem for proficiente com uma arma.
+function PersonagemProficienteComArma(nome_arma) {
+  // Verifica lista de armas.
+  return personagem.proficiencia_armas[nome_arma] != null;
+}
+
+// @return o valor do foco do personagem com a arma (0, 1, 2).
+// @param nome_arma chave da arma.
+// @param maior indica se o foco eh maior.
+function PersonagemFocoComArma(nome_arma) {
+  return personagem.foco_armas[nome_arma];
+}
+
+// @param nome_talento nome do talento na tabela ou chave na tabela.
+// @param complemento alguns talentos precisam de um complemento. Por exemplo,
+//        conhecimento (complemento), usar_arma_comum (complemento).
+// @return true se o personagem tiver o talento passado.
+function PersonagemPossuiTalento(nome_talento, complemento) {
+  for (var i = 0; i < personagem.talentos.lista.length; ++i) {
+    var chave_talento = personagem.talentos.lista[i].nome;
+    if (nome_talento == chave_talento ||
+        nome_talento == tabelas_talentos[chave_talento].nome) {
+      // TODO ver essa logica de complemento com calma.
+      if (tabelas_talentos[chave_talento].complemento && 
+          personagem.talentos.lista[i].complemento && complemento) {
+        // Trata complemento se houver.
+        if (personagem.talentos.lista[i].complemento == complemento) {
+          return true;
+        }
+      } else {
+        return true;
+      }
+    }
+  }
+  return false;
+}
+
+// @return true se o personagem possuir uma das classes passadas.
+// @param classes array de classe.
+function PersonagemPossuiUmaDasClasses(classes) {
+  for (var i = 0; i < personagem.classes.length; ++i) {
+    for (var j = 0; j < classes.length; ++j) {
+      if (classes[j] == personagem.classes[i].classe) {
+        return true;
+      }
+    }
+  }
+  return false;
+}
+
+// @return o nivel do personagem na classe passada, zero se nao possuir.
+function PersonagemNivelClasse(classe) {
+  for (var i = 0; i < personagem.classes.length; ++i) {
+    if (personagem.classes[i].classe == classe) {
+      return personagem.classes[i].nivel;
+    }
+  }
+  return 0;
+}
+
+// @return true se o personagem atender todos os requisitos do talento.
+function PersonagemVerificaPrerequisitosTalento(chave_talento, complemento) {
+  var requisitos = tabelas_talentos[chave_talento].requisitos;
+  var prefixo_erro = tabelas_talentos[chave_talento].nome + ' requer ';
+  if (requisitos == null) {
+    return true;
+  }
+  if (requisitos.bba) {
+    if (personagem.bba < requisitos.bba) {
+      alert(prefixo_erro + 'BBA >= ' + requisitos.bba);
+      return false;
+    }
+  }
+  for (var atributo in requisitos.atributos) {
+    if (personagem.atributos[atributo] < requisitos.atributos[atributo]) {
+      alert(prefixo_erro + atributo + ' >= ' + requisitos.atributos[atributo]);
+      return false;
+    }
+  }
+  for (var classe in requisitos.nivel) {
+    if (PersonagemNivelClasse(classe) < requisitos.nivel[classe]) {
+      alert(prefixo_erro + 'nivel em ' + classe + ' >= ' + requisitos.nivel[classe]);
+      return false;
+    }
+  }
+  for (var i = 0;  requisitos.talentos && i < requisitos.talentos.length; ++i) {
+    if (!PersonagemPossuiTalento(requisitos.talentos[i], complemento)) {
+      alert(prefixo_erro + 'talento ' + tabelas_talentos[requisitos.talentos[i]].nome + ' ' +
+          (complemento ? complemento : ''));
+      return false;
+    }
+  }
+  if (requisitos.proficiencia_arma && complemento) {
+    var chave_arma = tabelas_armas_invertida[complemento];
+    if (!PersonagemProficienteComArma(complemento)) {
+      alert(prefixo_erro + 'proficiencia com ' + complemento);
+      return false;
+    }
+  }
+  return true;
+}
