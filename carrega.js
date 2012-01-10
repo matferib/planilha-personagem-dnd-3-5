b04a834c43201d7665897c6fd416d38ddf33314a
Fernando C. de Carvalho Neto <fcarvalho@gmail.com>
2012-01-10 00:20:24 -0200

    correcao do converte_planilha.sh para funcionar no MacOS

---
+++ carrega.js
@@ -0,0 +1,152 @@
+// Funcoes de carregamento chamadas ao se carregar a pagina.
+
+// Chamado pelo carregamento inicial da pagina. Apesar de ser um tratador de eventos,
+// preferi manter neste arquivo ja que eh chamada apenas uma vez.
+function CarregamentoInicial() {
+  _CarregaBotoesVisao();
+  _CarregaAtributos();
+
+  // Monta a tabela de armas e cria as opcoes dinamicamente.
+  _CarregaTabelaArmas();
+  _CarregaPericias();
+
+  var indice_igual = document.URL.indexOf('=');
+  if (indice_igual != -1) {
+    // carrega pelos parametros. Caso contrario, usara a entrada padrao.
+    var json_personagem = decodeURIComponent(document.URL.slice(indice_igual + 1));
+    personagem = goog.json.parse(json_personagem);
+    AtualizaGeralSemConverterEntradas();
+  } else {
+    AtualizaGeralSemLerEntradas();
+  }
+}
+
+// Adiciona botoes dinamicamente na planilha.
+function _CarregaBotoesVisao() {
+  var div_visoes = Dom('div-visoes');
+  for (var visao in tabelas_visoes) {
+    var botao_visao = CriaSpan(tabelas_visoes[visao].nome, 'span-' + visao, null);
+    botao_visao.setAttribute('onclick', "ClickVisao('" + visao + "')");
+    div_visoes.appendChild(botao_visao);
+  }
+}
+
+function _CarregaAtributos() {
+  var div = Dom('div-stats');
+
+  div.appendChild(CriaSpan('Atributos'));
+  div.appendChild(CriaBr());
+  div.appendChild(CriaSpan('Total: '));
+  div.appendChild(CriaSpan('0', 'pontos-atributos-total'));
+  div.appendChild(CriaSpan(', gastos: [ '));
+  div.appendChild(CriaSpan('0', 'pontos-atributos-gastos'));
+  div.appendChild(CriaSpan(' ]'));
+  div.appendChild(
+      CriaBotao('-', 
+                'botao-atributos-menos', 
+                null, 
+                { handleEvent: function (evento) { 
+                    ClickBotaoAtributoMenos(); 
+                    evento.stopPropagation();
+                } }));
+
+  var atributos = { 
+      forca: 'Força', 
+      destreza: 'Destreza', 
+      constituicao: 'Constituicao', 
+      inteligencia: 'Inteligencia',
+      sabedoria: 'Sabedoria', 
+      carisma: 'Carisma' };
+  for (var chave_atributo in atributos) {
+    var div_atributo = CriaDiv();
+    div_atributo.appendChild(
+        CriaBotao('+', 
+                  'botao-atributos-mais-' + chave_atributo, 
+                  'botoes-atributos', 
+                  { 
+                      chave_atributo: chave_atributo,
+                      handleEvent: function (evento) { 
+                          ClickBotaoAtributoMais(this.chave_atributo); 
+                          evento.stopPropagation();
+                  } }));
+    div_atributo.appendChild(CriaSpan(atributos[chave_atributo]));
+    var input_atributo = CriaInputTexto('10', chave_atributo + '-valor-base');
+    input_atributo.size = 2;
+    input_atributo.maxLength = 2;
+    input_atributo.setAttribute('onchange', 'AtualizaGeral()');
+    div_atributo.appendChild(input_atributo);
+    div_atributo.appendChild(CriaSpan('0', chave_atributo + '-mod-racial'));
+    div_atributo.appendChild(CriaSpan('0', chave_atributo + '-mod-nivel'));
+    div_atributo.appendChild(CriaSpan(' = '));
+    div_atributo.appendChild(CriaSpan('0', chave_atributo + '-valor-total'));
+    div_atributo.appendChild(CriaSpan(', modificador: '));
+    div_atributo.appendChild(CriaSpan('0', null, chave_atributo + '-mod-total'));
+
+    div.appendChild(div_atributo);
+  }
+}
+
+
+// Preenche os nomes faltantes na tabela de armas e chama as funcoes
+// para preencher os selects de armas corpo a corpo e a distancia.
+function _CarregaTabelaArmas() {
+  var tabelas_armas_especificas = [ 
+      tabelas_armas_simples, tabelas_armas_comuns, tabelas_armas_exoticas ];
+  var talento_relacionado = [
+      'usar_armas_simples', 'usar_arma_comum', 'usar_arma_exotica' ];
+  for (var i = 0; i < tabelas_armas_especificas.length; ++i) {
+    var tabela_especifica = tabelas_armas_especificas[i];
+    for (var arma in tabela_especifica) {
+      // Primeiro, preenche o nome da arma.
+      if (tabela_especifica[arma].nome == null) {
+        tabela_especifica[arma].nome = arma;
+      }
+      tabela_especifica[arma].talento_relacionado = talento_relacionado[i];
+      // Compoe a tabela principal.
+      tabelas_armas[arma] = tabela_especifica[arma];
+      // Compoe a tabela invertida.
+      tabelas_armas_invertida[tabela_especifica[arma].nome] = arma;
+    }
+  }
+}
+
+// Popula as pericias iniciais.
+function _CarregaPericias() {
+  var div_pericias = Dom('div-pericias');
+  for (var chave_pericia in tabelas_pericias) {
+    var pericia = tabelas_pericias[chave_pericia];
+    var habilidade = pericia.habilidade;
+    var prefixo_id = 'pericia-' + chave_pericia;
+    var div = CriaDiv(prefixo_id);
+    div.appendChild(
+        CriaSpan(pericia.nome + ' (' + pericia.habilidade + '): ', null, 
+                 'pericias-nome'));
+    var input_pontos =
+        CriaInputNumerico('0', prefixo_id + '-pontos', null, 
+        { chave_pericia: chave_pericia,
+          handleEvent: function(evento) {
+            ClickPericia(this.chave_pericia);
+            evento.stopPropagation(); } });
+    input_pontos.min = 0;
+    input_pontos.maxlength = input_pontos.size = 2;
+    div.appendChild(input_pontos);
+
+    div.appendChild(CriaSpan(' pontos; '));
+    div.appendChild(CriaSpan('0', prefixo_id + '-graduacoes'));
+    div.appendChild(CriaSpan('+0', null, habilidade + '-mod-total'));
+    div.appendChild(CriaSpan('+0', prefixo_id + '-sinergia'));
+    div.appendChild(CriaSpan('+0', prefixo_id + '-bonus-talento'));
+    div.appendChild(CriaSpan(' = '));
+    div.appendChild(CriaSpan('+0', prefixo_id + '-total'));
+
+    // Adiciona ao div.
+    div_pericias.appendChild(div);
+
+    // Adiciona as entradas
+    entradas.pericias.push({ chave: chave_pericia, pontos: 0 });
+    // Adiciona ao personagem.
+    personagem.pericias.lista[chave_pericia] = {
+        graduacoes: 0, bonus_habilidade: 0, bonus_talentos: {}, bonus_sinergia: 0
+    };
+  }
+}
---
+++ dom.js
@@ -0,0 +1,163 @@
+// Funcoes uteis relacionadas a dom.
+
+// @return o dom com o id passado.
+function Dom(id) {
+  return document.getElementById(id);
+}
+
+function CriaDom(tipo, id, classe) {
+  var dom_criado = document.createElement(tipo);
+  if (id) {
+    dom_criado.id = id;
+  }
+  if (classe) {
+    dom_criado.className = classe;
+  }
+  return dom_criado;
+}
+
+// @return um dom <br>.
+function CriaBr() {
+  return CriaDom('br');
+}
+
+function CriaOptGroup(rotulo) {
+  var optgroup = CriaDom('optgroup');
+  optgroup.label = rotulo;
+  return optgroup;
+}
+
+function CriaOption() {
+  return CriaDom('option');
+}
+
+function CriaBotao(texto, id, classe, funcao) {
+  var botao = CriaDom('button', id, classe);
+  botao.setAttribute('type', 'button');
+  botao.textContent = texto;
+  if (funcao) {
+    botao.addEventListener('click', funcao, false);
+  }
+  return botao;
+}
+
+function CriaRadio(texto, id, classe, nome_grupo, funcao) {
+  var botao = CriaDom('input', id, classe);
+  botao.setAttribute('type', 'radio');
+  botao.name = nome_grupo;
+  // botao.textContent = texto;
+  if (funcao) {
+    botao.addEventListener('click', funcao, false);
+  }
+  return botao;
+}
+
+function CriaDiv(id, classe) {
+  return CriaDom('div', id, classe);
+}
+
+function CriaSpan(texto, id, classe) {
+  var span = CriaDom('span', id, classe);
+  if (texto) {
+    span.textContent = texto;
+  }
+  return span;
+}
+
+// @param marcado true se o checkbox estiver marcado.
+function CriaInputCheckbox(marcado, id, classe) {
+  var input = CriaDom('input', id, classe);
+  input.type = 'checkbox';
+  input.checked = marcado;
+  return input;
+}
+
+// Cria um input de texto com os atributos passados.
+// @param texto o texto mostrado dentro do input.
+// @param id do input.
+// @param classe do input.
+function CriaInputTexto(texto, id, classe) {
+  var input = CriaDom('input', id, classe);
+  input.type = 'text';
+  if (texto) {
+    input.value = texto;
+  }
+  return input;
+}
+
+// Cria um input de numero com os atributos passados.
+// @param numero o numero mostrado dentro do input.
+// @param id do input.
+// @param classe do input.
+// @param funcao ou handler a ser chamado.
+function CriaInputNumerico(numero, id, classe, funcao) {
+  var input = CriaDom('input', id, classe);
+  input.type = 'number';
+  if (numero) {
+    input.value = numero;
+  }
+  if (funcao) {
+    //input.addEventListener('onchange', funcao, false);
+    input.addEventListener('input', funcao, false);
+  }
+  return input;
+}
+
+function CriaSelect(id, classe) {
+  var select = CriaDom('select', id, classe);
+  return select;
+}
+
+// Cria um span com os botoes mais e menos, retornando-o.
+// @param id do input de texto com o valor do botao.
+// @param classe do input com o valor do botao.
+// @param nome_funcao a ser chamada pelos botoes, que deve ser do tipo
+//        funcao(string, incremento).
+// @param parametro a ser passado para a funcao.
+function CriaBotoesMaisMenos(id, classe, nome_funcao, parametro) {
+  var span = CriaSpan();
+  var botao_mais = CriaBotao('+');
+  botao_mais.setAttribute('onclick', nome_funcao + "('" + parametro + "', 1)");
+  span.appendChild(botao_mais);
+  var botao_menos = CriaBotao('-');
+  botao_menos.setAttribute('onclick', nome_funcao + "('" + parametro + "', -1)");
+  span.appendChild(botao_menos);
+  var input_pontos = CriaInputTexto('0', id, classe);
+  input_pontos.size = 2;
+  input_pontos.readOnly = true;
+  span.appendChild(input_pontos);
+  return span;
+}
+
+// Remove o filho do pai com o id passado.
+// @param id_filho identificador do filho.
+// @param pai elemento que contem os filhos.
+function RemoveFilho(id_filho, pai) {
+  for (var i = 0; i < pai.childNodes.length; ++i) {
+    var filho = pai.childNodes[i];
+    if (filho.id == id_filho) {
+      pai.removeChild(filho);
+    }
+  }
+}
+
+// Remove os filhos de um dom. Antes, remove todos os onchange do elemento
+// para evitar chamadas de onchange durante a remocao. 
+function RemoveFilhos(dom) {
+  if (dom == null) {
+    return;
+  }
+  _RemoveOnChange(dom, false);
+  goog.dom.removeChildren(dom);
+}
+
+// Remove o atributo onchange do elemento e seus filhos.
+// @param dom que tera onchange removido e de seus filhos.
+function _RemoveOnChange(dom) {
+  for (var filho = dom.firstChild; filho != null; filho = filho.nextSibling) {
+    if (filho.removeAttribute) {
+      filho.removeAttribute('onchange');
+      _RemoveOnChange(filho);
+    }
+  }
+}
