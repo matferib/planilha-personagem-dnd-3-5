2acd482d4b46c97e6336446d8c8e92f2d9bdd1ee
Fernando C. de Carvalho Neto <fcarvalho@gmail.com>
2012-01-10 00:20:24 -0200

    correcao do converte_planilha.sh para funcionar no MacOS

--- converte_planilha.sh
+++ converte_planilha.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 # Alterna planilha.html entre versao compilada e nao compilada.
 
-sed planilha.html -e 's/<script type/<\!--scriptian type/' -e 's/<\/script>/<\/scriptian-->/' -e 's/\!\-\-script type/script type/' -e 's/script\-\-/script/' -e 's/scriptian/script/g' > planilha.html.temp
+sed -e 's/<script type/<\!--scriptian type/' -e 's/<\/script>/<\/scriptian-->/' -e 's/\!\-\-script type/script type/' -e 's/script\-\-/script/' -e 's/scriptian/script/g' planilha.html > planilha.html.temp
 mv planilha.html.temp planilha.html
