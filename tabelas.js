d894d461477da910b2d4bedbba7bc6a5af529017
Matheus Ribeiro <mfribeiro@gmail.com>
2012-01-28 14:39:57 -0200

    comecei a fazer a geracao automatica de personagem

--- tabelas.js
+++ tabelas.js
@@ -226,30 +226,6 @@ function bba_nulo() {
   return 0;
 }
 
-// Tabelas de geracao de atributos.
-var tabelas_geracao_atributos = {
-  // Peguei do livro do mestre.
-  barbaro: ['forca', 'destreza', 'constituicao', 'sabedoria', 'inteligencia', 'carisma' ],
-  bardo: [ 'carisma', 'inteligencia', 'destreza', 'constituicao', 'forca', 'sabedoria' ],
-  clerigo: [ 'sabedoria', 'constituicao', 'forca', 'carisma', 'inteligencia',  'destreza' ],
-  druida: [ 'sabedoria', 'destreza', 'constituicao', 'inteligencia', 'forca', 'carisma' ],
-  guerreiro: [ 'forca', 'constituicao', 'destreza', 'sabedoria', 'inteligencia', 'carisma' ],
-  feiticeiro: [ 'carisma', 'destreza', 'constituicao', 'sabedoria', 'inteligencia', 'forca' ],
-  ladino: [ 'destreza', 'inteligencia', 'constituicao', 'forca', 'sabedoria', 'carisma' ],
-  mago: [ 'inteligencia', 'destreza', 'constituicao', 'sabedoria', 'forca', 'carisma' ],
-  monge: [ 'sabedoria', 'forca', 'destreza', 'constituicao', 'inteligencia', 'carisma' ],
-  paladino: [ 'carisma', 'forca', 'sabedoria', 'constituicao', 'inteligencia', 'destreza' ],
-  ranger: [ 'destreza', 'forca', 'constituicao', 'sabedoria', 'inteligencia', 'carisma' ],
-  // classes NPC: nao existe uma tabela para esses, coloquei o que achei mais adequado.
-  adepto: [ 'sabedoria', 'destreza', 'constituicao', 'inteligencia', 'forca', 'carisma' ],
-    // Esse ta muito ruim...
-  aristocrata: [ 'destreza', 'inteligencia', 'forca', 'carisma', 'constituicao', 'sabedoria' ],
-  plebeu: [ 'forca', 'constituicao', 'sabedoria', 'destreza', 'inteligencia', 'carisma' ],
-    // esse aqui varia de acordo com as escolhas da area de expertise.
-  expert: [ 'inteligencia', 'forca', 'destreza', 'constituicao', 'sabedoria', 'carisma' ],
-  combatente: [  'forca', 'constituicao', 'destreza', 'sabedoria', 'inteligencia', 'carisma' ],
-};
-
 // Atributos:
 // Modificador.
 function modificador_atributo(valor_atributo) {
