9f1e0142acbe40bef96bf6b423145959e3e3d6dd
Matheus Ribeiro <mfribeiro@gmail.com>
2013-06-12 00:53:37 -0300

    mexi nos scripts

--- gera_packaged_app.sh
+++ gera_packaged_app.sh
@@ -3,3 +3,5 @@
 # Converte a planilha se necessario
 grep 'tudo\.js' planilha.html | grep -q '!--' && ./converte_planilha.sh || echo 'Conversão desnecessária'
 ./geratudo.sh && zip planilha.zip planilha.html tudo.js planilha.css manifest.json icon16.png icon128.png background.js 
+# Desfaz a conversao (sempre, pois se nao tiver feito é porque ja estava feito antes).
+./converte_planilha.sh
