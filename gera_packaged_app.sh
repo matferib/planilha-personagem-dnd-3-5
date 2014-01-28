25297e2444c14ea80cd74ab177929fe832b6d8ca
Matheus Ribeiro <mfribeiro@gmail.com>
2014-01-27 23:48:49 -0200

    adicionei rm no gera packaged app

--- gera_packaged_app.sh
+++ gera_packaged_app.sh
@@ -2,6 +2,8 @@
 
 # Converte a planilha se necessario
 grep 'tudo\.js' planilha.html | grep -q '!--' && ./converte_planilha.sh || echo 'Conversão desnecessária'
-./geratudo.sh && zip planilha.zip planilha.html tudo.js planilha.css manifest.json icon16.png icon128.png background.js 
+./geratudo.sh && \
+rm -f planilha.zip && \
+zip planilha.zip planilha.html tudo.js planilha.css manifest.json icon16.png icon128.png background.js 
 # Desfaz a conversao (sempre, pois se nao tiver feito é porque ja estava feito antes).
 ./converte_planilha.sh
