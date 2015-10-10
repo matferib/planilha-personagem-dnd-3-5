aee36c2d79ad0d226b91c2ecb1a541c4d1f7ca53
Matheus Ribeiro <mfribeiro@gmail.com>
2015-10-10 19:03:13 -0300

    pequenos fixes para lancar 0_8_0

--- gera_packaged_app.sh
+++ gera_packaged_app.sh
@@ -4,7 +4,7 @@
 grep 'tudo\.js' planilha.html | grep -q '!--' && ./converte_planilha.sh || echo 'Conversão desnecessária'
 ./geratudo.sh && \
 rm -f planilha.zip && \
-zip -r planilha.zip planilha.html tudo.js planilha.css manifest.json icon16.png icon128.png background.js _locales
+zip -r planilha.zip planilha.html tudo.js planilha.css manifest.json icon16.png icon128.png background.js _locales 
 pushd . && rm -Rf package/* && mkdir -p package && cd package && unzip ../planilha.zip
 popd
 # Desfaz a conversao (sempre, pois se nao tiver feito é porque ja estava feito antes).
--- geratudo.sh
+++ geratudo.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
-CALCDEPS="${CLOSURELIB:-../closure-library}/closure/bin/calcdeps.py"
+CALCDEPS="${CLOSURELIB:=../closure-library}/closure/bin/calcdeps.py"
 
 rm -f tudo.js tudo.temp.js
 
-${CALCDEPS} -i adiciona.js -i atualiza.js -i bonus.js -i carrega.js -i converte.js -i dom.js -i entradas.js -i eventos.js -i geracao.js -i personagem.js -i tabelas.js -i util.js -i tabelas_habilidades.js -i tabelas_geracao.js -i tabelas_feiticos.js -i dependencias.js -i estado.js -i entradas.js -i dom_planilha.js -p "${CLOSURELIB}"  -o script > tudo.temp.js
+#${CALCDEPS} -i adiciona.js -i atualiza.js -i bonus.js -i carrega.js -i converte.js -i dom.js -i entradas.js -i eventos.js -i geracao.js -i personagem.js -i tabelas.js -i util.js -i tabelas_habilidades.js -i tabelas_geracao.js -i tabelas_feiticos.js -i dependencias.js -i estado.js -i entradas.js -i dom_planilha.js -p "${CLOSURELIB}"  -o script > tudo.temp.js
+cat adiciona.js atualiza.js bonus.js carrega.js converte.js dom.js entradas.js eventos.js geracao.js personagem.js tabelas.js util.js tabelas_habilidades.js tabelas_geracao.js tabelas_feiticos.js dependencias.js estado.js entradas.js dom_planilha.js > tudo.js
 
 # Here we change goog.writeScriptTag_ function so that it does not call doc.write
 # since it gives us trouble in packed apps.
 
-sed -e 's/doc.write/var not_used = parseInt/g' tudo.temp.js > tudo.js && rm tudo.temp.js
+#sed -e 's/doc.write/var not_used = parseInt/g' tudo.temp.js > tudo.js && rm tudo.temp.js
